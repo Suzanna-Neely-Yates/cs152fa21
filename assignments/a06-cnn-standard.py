1aa1bd6ed201f39547f02f38b08b45bd9677e58b
Suzanna Neely Yates <nyates@students.pitzer.edu>
2021-11-14 12:40:03 -0800

    edit parameters

--- assignments/a06-cnn-standard.py
+++ assignments/a06-cnn-standard.py
@@ -114,8 +114,10 @@ def main():
 
     model = models.resnet18(pretrained=False, progress=True)
 
-    model.conv1 = torch.nn.Conv2d(1, 28, kernel_size=3, padding=1)
-    model.fc = torch.nn.Linear(10976, 10),
+    #model.conv1 = torch.nn.Conv2d(1, 28, kernel_size=3, padding=1)
+    model.fc = torch.nn.Linear(in_features=384, out_features=10, bias=True)
+
+    #model.fc = nn.Linear(in_features=512, out_features=10, bias=True)
 
     """
     model = torch.nn.Sequential(
