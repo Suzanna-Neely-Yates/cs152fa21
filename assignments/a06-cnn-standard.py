3d0e12f02e01efcc260572fa9b0cc311e258fc5f
Suzanna Neely Yates <nyates@students.pitzer.edu>
2021-11-14 12:43:40 -0800

    debug

--- assignments/a06-cnn-standard.py
+++ assignments/a06-cnn-standard.py
@@ -114,7 +114,7 @@ def main():
 
     model = models.resnet18(pretrained=False, progress=True)
     model.conv1 = torch.nn.Conv2d(1, 28, kernel_size=3, padding=1)
-    print(model.conv1)
+    print(shape[model.conv1])
     model.fc = torch.nn.Linear(in_features=28, out_features=10, bias=True)
 
     #model.fc = nn.Linear(in_features=512, out_features=10, bias=True)
